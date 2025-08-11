5a50422b0b1dcf3b45ef4e7686a20fc51a20058d
zerobytev <ilyaberdikov67@gmail.com>
2025-08-11 20:20:45 +0300

    Update 7sd8qdu2d3322qz0332131243.lua

--- 7sd8qdu2d3322qz0332131243.lua
+++ 7sd8qdu2d3322qz0332131243.lua
@@ -1877,10 +1877,11 @@ local phobia = {
 			local servs = {}
 
 			local group = {
-				a = pui.group("AA", "Fake Lag"),
+				a = pui.group("AA", "anti-aimbot angles"),
 				f = pui.group("AA", "Other"),
-				o = pui.group("AA", "anti-aimbot angles"),
+				o = pui.group("AA", "Fake Lag"),
 			}
+
 
 			for k, v in pairs(self.servers.gen) do
 				table.insert(servs, k)
@@ -3669,5 +3670,6 @@ end
 cvar.con_filter_enable:set_int(1)
 cvar.con_filter_text:set_string("IrWL5106TZZKNFPz4P4Gl3pSN?J370f5hi373ZjPg%VOVh6lN")
 client.exec("con_filter_enable 1")
+
 
 
